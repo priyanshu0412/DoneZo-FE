ff02003e1bb644f3b235831b919267495aa0a0c7
priyanshu0412 <pdevops04@gmail.com>
2025-06-28 20:35:35 +0530

    initial commit from new

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 // -----------------------------------------
@@ -8,10 +9,12 @@ const Navbar = () => {
             <div className="w-full h-20 flex justify-center items-center backdrop-blur-sm bg-transparent">
                 <div className="max-w-[1200px] flex h-full w-full justify-between items-center px-8 lg:px-0">
                     <div className="text-white text-2xl font-bold ">
-                        DoneZo
+                        <Link href={"/"}>
+                            DoneZo
+                        </Link>
                     </div>
-                    <div className="flex items-center ">
-                        Dont Have Account
+                    <div className="flex items-center text-white">
+                        Dont Have Account ?
                     </div>
                 </div>
             </div>
